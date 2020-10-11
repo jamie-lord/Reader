a7055fb4638dc9788b54b20cae31198d68e64678
Jamie Lord <jamie@lord.technology>
2020-10-11 21:06:10 +0100

    Use existing refresh method

--- Reader/Services/FeedsService.cs
+++ Reader/Services/FeedsService.cs
@@ -43,38 +43,10 @@ namespace Reader.Services
                 {
                     try
                     {
-                        var f = await FeedReader.ReadAsync(feed.Uri);
-
-                        feed.LastChecked = DateTime.Now;
-                        _context.Feeds.Update(feed);
-                        await _context.SaveChangesAsync();
-
-                        foreach (var item in f.Items)
-                        {
-                            if (_context.Items.Any(x => x.Uri == item.Link))
-                            {
-                                // Item already exists from previous fetch or another feed
-                                continue;
-                            }
-
-                            var newItem = new Item
-                            {
-                                Author = item.Author,
-                                Categories = item.Categories?.ToList(),
-                                Content = item.Content,
-                                Description = item.Description,
-                                Feed = feed,
-                                Published = item.PublishingDate,
-                                Title = item.Title,
-                                Uri = item.Link
-                            };
-                            _context.Items.Add(newItem);
-                            await _context.SaveChangesAsync();
-                        }
+                        await RefreshFeed(feed.Id);
                     }
                     catch (Exception ex)
                     {
-
                     }
                 }
 
