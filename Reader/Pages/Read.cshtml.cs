57f9b0396a308364773cb1dc0b57a3e020d1a705
Jamie Lord <jamie@lord.technology>
2020-10-17 09:58:12 +0100

    Code cleanup

--- Reader/Pages/Read.cshtml.cs
+++ Reader/Pages/Read.cshtml.cs
@@ -1,9 +1,9 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Reader.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Reader.Pages
 {
--- Reader/Pages/Unread.cshtml.cs
+++ Reader/Pages/Unread.cshtml.cs
@@ -1,10 +1,10 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 using Reader.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Reader.Pages
 {
